14d3f56cb716a1d95fad31902dd094ccbd0a3afd
YuukaDev <vule.amv1@gmail.com>
2021-11-21 21:45:20 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Header from "./components/Header";
 import Form from "./components/Form";
 
 function App() {
   return (
     <div>
+      <Header />
       <Form />
     </div>
   );
