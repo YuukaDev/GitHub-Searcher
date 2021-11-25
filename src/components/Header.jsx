1807af936a8a1019ccf490cc4026b7e6e9f3d588
YuukaDev <vule.amv1@gmail.com>
2021-11-25 17:23:40 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function Header() {
-    return (
-        <header>
-            <h1 className="display-1 text-center">GitHub Searcher</h1>
-        </header>
-    )
+  return (
+    <header>
+      <h1 className="display-1 text-center">GitHub Searcher</h1>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
