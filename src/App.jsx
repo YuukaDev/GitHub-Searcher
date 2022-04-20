955091d3ff2ee86e0016efed42c6e7a2a44dcca4
YuukaDev <vule.amv1@gmail.com>
2022-04-20 22:03:22 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from "./components/Header.jsx";
 import Form from "./components/Form.jsx";
+import Image from "../src/images/search.png";
+import Repos from "./components/repos";
 
 function App() {
   return (
     <main>
-      <Header />
-      <Form />
+      <Repos />
     </main>
   );
 }
