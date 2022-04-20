24d5109b728322c5aadb3bf63498c58481f96722
YuukaDev <vule.amv1@gmail.com>
2022-04-20 22:03:23 +0200

    Create repos.jsx

---
+++ src/components/repos.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+
+export default function Repos() {
+  const getUser = async (username) => {
+    try {
+      const url = await fetch(`https://api.github.com/users/${username}/repos`);
+      const random = await url.json();
+      setBase(random);
+      console.log(random);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getUser("YuukaDev");
+  }, []);
+
+  const [base, setBase] = useState(null);
+  return (
+    <>
+      <h1>Repository</h1>
+      <div>
+        {base.map((repos, index) => (
+          <h1>{repos.name}</h1>
+        ))}
+      </div>
+    </>
+  );
+}
